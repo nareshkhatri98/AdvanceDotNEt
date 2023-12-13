77f5c813418ab9a353439e678e5c1cf305adf6cf
Naresh Khatri <111528185+nareshtestgithub@users.noreply.github.com>
2023-12-13 07:12:24 +0545

    first commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+
+namespace AdvanceDotnet
+{
+    //exception is any kind of error that doesn't allow to run the code below it i.e it distrub the normal flow of
+    // program . Exception handiling is a machanism that handle such error such that normal flow of program can be
+    // maintained the keword used in exception handling are
+    // try: use to surround the code that can generate error.
+    // catch: use to handle error thrown from the catch block.
+    // throw: use to explicitly throw excepltion for any place.
+    // finally : finally block is always executed.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //example on try-catch bu handling : divde by 0 exception..
+            try
+            {
+                Console.WriteLine("Enter first number");
+                int num1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second number");
+                int num2 = Int32.Parse(Console.ReadLine());
+                int Div = num1 / num2;
+                Console.WriteLine("Division is " + Div);
+
+                // handiling indexoutof bounds exception : if 
+                // attempt is made to insert data beyound the size of array.
+
+                int[] arr1 = new int[5];// size of array is 5.
+
+                arr1[0] = 1;
+                arr1[1] = 2;
+                arr1[2] = 4;
+                arr1[3] = 8;
+                arr1[4] = 9;
+                arr1[5] = 10;// out of range..
+                                
+            }
+            
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("denomenator can't be 0" +ex);
+            }
+            catch(IndexOutOfRangeException i)
+            {
+                Console.WriteLine("size of array is executed.." +i);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("this is finally block..");
+                Console.WriteLine("it is always executed...");
+            }
+
+            Console.WriteLine("result is generated");
+            Console.WriteLine("hllo bca 5th sem");
+
+            // example own exception (user define exception..)..
+            Console.WriteLine("--example on user defined exception -- ");
+
+            ownExceptionDemo obj = new ownExceptionDemo();
+            try
+            {
+                obj.sendYourage(14);
+            }catch(AgeException a)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ownExceptionDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AdvanceDotnet
+{
+    // we can create our own exception type(user define)
+    //exception by inheriting  Exception class
+    // Step1: Create Error class by inheriting eception class
+    //step2:write a program and throw own error. 
+    //Step3: handle with try catch..
+
+    // creating error class..
+    class AgeException : Exception
+    {
+        // if age is less than 16 throw your error..
+        public AgeException(string s):base(s)
+        {
+            Console.WriteLine("age cannot be less the 16..");
+        }
+    }
+    class ownExceptionDemo
+    {
+        public void sendYourage( int age)
+        {
+           if(age < 16)
+            {
+                //throw own exception
+                throw new AgeException("age  can't be less tha 16");
+            }
+            else
+            {
+                Console.WriteLine("your are eligible  to participate");
+            }
+        }
+
+    }
+}
